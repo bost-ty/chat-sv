3f9e14cfe0d9ce9fe4c429cd21cd2463c70431a1
bost-ty <61333914+bost-ty@users.noreply.github.com>
2025-03-04 12:39:25 -0800

    feature: connect and disconnect to many channels

--- src/App.svelte
+++ src/App.svelte
@@ -4,45 +4,47 @@
 
 	let targetChannel = $state("");
 	let value = $state("");
-	let submitted = $state(false);
 	let isHovered = $state(false);
+
+	let channels = $state([]);
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		if (!submitted) {
-			submitted = true;
-			targetChannel = value.toLowerCase();
-		} else {
-			submitted = false;
-			value = "";
-			targetChannel = value;
-		}
+		targetChannel = value.toLowerCase();
+		channels.push(targetChannel);
+		value = "";
+	}
+
+	function disconnectChannel(channel) {
+		channels = channels.filter((ch) => ch !== channel);
 	}
 </script>
 
 <!-- This whole <section> could be extracted into a component and reused! -->
 <section>
 	<form onsubmit={handleSubmit}>
-		<label
-			>Enter channel name: <input
-				type="text"
-				placeholder="AlveusSanctuary"
-				bind:value
-				disabled={submitted}
-			/>
-		</label>
-		<button type="submit">{!submitted ? "Connect" : "Disconnect"}</button>
+		<label>Enter channel name: <input type="text" bind:value /> </label>
+		<button type="submit">Connect to channel</button>
 	</form>
-	{#if submitted}
-		<h2 transition:fade={{ duration: 100 }}>{targetChannel}</h2>
-		<Chat {targetChannel} bind:isHovered />
-	{/if}
-
-	{#if isHovered}
-		<div transition:fade={{ duration: 100 }} class="hoverMessage">
-			Scroll paused due to hover
-		</div>
-	{/if}
+	<div>
+		{#each channels as targetChannel (targetChannel)}
+			<div
+				transition:fade={{ duration: 100 }}
+				style="display: flex; align-items: center; gap: 1rem;"
+			>
+				<h2>{targetChannel}</h2>
+				<button type="button" onclick={() => disconnectChannel(targetChannel)}
+					>Disconnect</button
+				>
+			</div>
+			<Chat {targetChannel} bind:isHovered />
+			{#if isHovered}
+				<div transition:fade={{ duration: 100 }} class="hoverMessage">
+					Scroll paused due to hover
+				</div>
+			{/if}
+		{/each}
+	</div>
 </section>
 
 <style>
